b041715e411db8723b82bae2b1b9a1d0e26734c6
Christopher Stott <christopherstott@gmail.com>
2015-09-03 09:38:03 -0700

    Initial

---
+++ BBTestAppCustomPostbuild/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BBTestAppCustomPostbuild
+//
+//  Created by Chris on 2015-09-03.
+//  Copyright (c) 2015 Chris. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BBTestAppCustomPostbuild/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  BBTestAppCustomPostbuild
+//
+//  Created by Chris on 2015-09-03.
+//  Copyright (c) 2015 Chris. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
